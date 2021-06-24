d098e4d76df7e1a4c0123b3e78c72654bf96a8a5
Loginov-Mikhail <78172722+Loginov-Mikhail@users.noreply.github.com>
2021-06-24 10:36:41 +0300

    Пятая версия - налогобложение согласно правилам с 01.01.2021

--- bonds.py
+++ bonds.py
@@ -8,76 +8,25 @@
 # html5lib.
 #   При выполнении в командной строке:
 #   python3 bonds.py
-#                Copyright (c) 2018 - 2020 Логинов М.Д.
+#                Copyright (c) 2018 - 2021 Логинов М.Д.
 #  Разработчик: Логинов М.Д.
-#  Модифицирован: 29 апреля 2020 г.
+#  Модифицирован: 23 июня 2021 г.
 # ******************************************************************************
 
 # -*- coding: utf-8 -*-
 import urllib.request
 import datetime
 import pandas as pd
-import time
-
-# Ключевая ставка ЦБ РФ.
-KEY_RATE = 6
-# Дата, с которой купоны корпоративных облигаций освобождены от НДФЛ, если
-# ставка купона не превышает на 5 п.п. ключевую ставку.
-FREE_TAX = datetime.datetime(2017, 1, 1)
 
 
 # ******************************************************************************
 # Процедура сохранения страницы во временный файл
 # ******************************************************************************
 def load_url(url):
-    time.sleep(0.5)
     response = urllib.request.urlopen(url).read().decode('cp1251')
     f = open('temp.html', 'w', encoding='cp1251')
     f.write(response)
     f.close()
-
-
-# ******************************************************************************
-# Функция определения налогового коэффициента. Для государственных и муници-
-# пальных облигаций возвращает 0, в остальных случаях - 1.
-# ******************************************************************************
-def tax_rate(strName):
-    result = "1"
-    if strName.lower().count('министерство') > 0:
-        result = "0"
-    if strName.lower().count('республика') > 0:
-        result = "0"
-    if strName.lower().count('край') > 0:
-        result = "0"
-    if strName.lower().count('область') > 0:
-        result = "0"
-    return result
-
-
-# ******************************************************************************
-# Функция определения суммы налога подлежащего уплате.
-# ******************************************************************************
-def tax(coupon, rate, taxRating, datePlacement):
-    if taxRating > 0:
-        arrayDate = datePlacement.split('.')
-        dtDate = datetime.datetime(int(arrayDate[2]), int(arrayDate[1]),
-                                   int(arrayDate[0]))
-        if dtDate > FREE_TAX:
-            if KEY_RATE + 5 >= rate:
-                result = 0
-            else:
-                standard = (KEY_RATE + 5) * coupon / rate
-                result = 0.35 * (coupon - standard)
-        else:
-            if KEY_RATE + 5 >= rate:
-                result = 0.13 * coupon
-            else:
-                standard = (KEY_RATE + 5) * coupon / rate
-                result = 0.13 * standard + 0.35 * (coupon - standard)
-    else:
-        result = 0
-    result = int(100 * result) / 100
-    return result
 
 
 # ******************************************************************************
@@ -94,15 +43,8 @@ def parse_info(fileName):
         if i == 1:
             codeISIN = line.split('span>')[1][:-2]  # ISIN код
             i = 0
-        if i == 2:
-            placementDate = line.split('span>')[1][:-2]  # Дата размещения
-            i = 0
         if i == 3:
             redemption = line.split('span>')[1][:-2]  # Дата погашения
-            i = 0
-        if i == 4:
-            fullName = line.split('td>')[1][:-2]  # Полное наименование
-            taxRate = tax_rate(fullName)
             i = 0
         if line.count('h3') > 0:
             line2 = line.replace('>', '<')
@@ -110,43 +52,19 @@ def parse_info(fileName):
             name = str1[2]  # Название облигации
         if line.count('ISIN код:') > 0:
             i = 1
-        if line.count('Дата начала размещения:') > 0:
-            i = 2
         if line.count('Дата погашения:') > 0:
             i = 3
         if line.count('Номинал:') > 0:  # Номинал
             nominal = int(line.split('span>')[1][:-2].replace('\xa0', ''))
-        if line.count('Полное&nbsp;наименование') > 0:
-            i = 4
     f.close()
-    return name + ';' + codeISIN + ';' + placementDate + ';' + redemption + \
-        ';' + str(nominal) + ';' + taxRate
+    return name + ';' + codeISIN + ';' + redemption + ';' + str(nominal)
 
 
 # ******************************************************************************
 # Функция парсинга страниц bonds.finam.ru с информацией о платежах по облига-
 # ции. Результат - строка с совокупным купонным доходом, очищенным от налогов.
 # ******************************************************************************
-def parse_payments2(fileName, taxRating, datePlacement):
-    f = open(fileName, 'r', encoding='cp1251')
-    sumPayments = 0
-    for line in f:
-        if line.count('<table border=0 cellspacing=0') > 0:
-            for row in line.split('<tr'):
-                if row.count('bline bg') > 0:
-                    cell = row.split('<td')
-                    rate = float(cell[3].replace('>', '&').replace(',', '.').
-                                 split('&')[1][:-1])  # Ставка
-                    coupon = float(cell[5].replace('>', '&').replace(',',
-                                   '.').split('&')[1])  # Купон
-                    sumPayments = sumPayments + coupon - tax(coupon, rate,
-                                                             taxRating,
-                                                             datePlacement)
-    f.close()
-    return str(sumPayments)
-
-
-def parse_payments(fileName, taxRating, datePlacement):
+def parse_payments(fileName):
     # Поиск таблицы с данными о платежах
     for i in range(8):
         df = pd.read_html(fileName)[i]
@@ -170,57 +88,9 @@ def parse_payments(fileName, taxRating, datePlacement):
     # Вычисление совокупного купонного дохода
     sumPayments = 0
     for i in range(len(df)):
-        sumPayments += df.loc[i, 'coupon'] - tax(df.loc[i, 'coupon'],
-                                                 df.loc[i, 'rate'],
-                                                 taxRating, datePlacement)
-        lastDate = df.loc[i, 'date']
+        sumPayments += 0.87 * df.loc[i, 'coupon']
     result = '%10.2f' % sumPayments
-    return result.strip() + ';' + lastDate.strftime('%d.%m.%Y')
-
-
-# ******************************************************************************
-# Функция парсинга страниц bonds.finam.ru с информацией о результатах торгов по
-# облигации. Результат - строка с ценой и НКД. Данные разделены точкой с
-# запятой.
-# ******************************************************************************
-def parse_auction_result(fileName):
-    f = open(fileName, 'r', encoding='cp1251')
-    i = 0
-    closePrice = 200
-    askPrice = 200
-    ACI = 200
-    for line in f:
-        if i == 1:
-            row = line.replace('>', '<').split('<')[2].split('&')[0]
-            if row.count('--') > 0:
-                closePrice = 200
-            else:
-                closePrice = float(row.replace(',', '.'))
-            i = 0
-        if i == 2:
-            row = line.replace('>', '<').split('<')[2].split('&')[0]
-            if row.count('--') > 0:
-                askPrice = 200
-            else:
-                askPrice = float(row.replace(',', '.'))
-            i = 0
-        if i == 3:  # НКД
-            row = line.replace('>', '<').split('<')[2].split('&')[0]
-            if row.count('--') > 0:
-                ACI = 200
-            else:
-                ACI = float(row.replace(',', '.'))
-            break
-        if line.count('lose:') > 0:
-            i = 1
-        if line.count('Ask:') > 0:
-            i = 2
-        if line.count('&nbsp;НКД&nbsp;(руб):') > 0:
-            i = 3
-    f.close()
-    if (closePrice == 200) or (closePrice < askPrice):
-        closePrice = askPrice  # Цена закрытия
-    return str(closePrice) + ';' + str(ACI)
+    return result.strip()
 
 
 # ******************************************************************************
@@ -231,21 +101,14 @@ fileInput = open('input.txt', 'r')
 fileOutput = open('output.txt', 'w')
 i = 0
 for line in fileInput:
-    url = line.replace('/default.asp', '00001/default.asp')
-    load_url(url)
-    newData = parse_info('temp.html')
-    arrInfo = newData.split(';')
     url = line.replace('/default.asp', '00002/default.asp')
     load_url(url)
-    payments = parse_payments('temp.html', int(arrInfo[5]), arrInfo[2])
-    result = newData.replace(arrInfo[2] + ';', '')[:-1]
-    url = line.replace('/default.asp', '00008/default.asp')
-    load_url(url)
-    newData = parse_auction_result('temp.html')
+    newData = parse_info('temp.html')
+    payments = parse_payments('temp.html')
+    result = newData
     i = i + 1
-    result = result + payments.replace('.', ',') + ';' + newData.replace('.',
-                                                                         ',')
-    print(str(i) + ';' + result)
-    fileOutput.write(result + '\n')
+    result = result + ';' + payments.replace('.', ',') + ';' + url
+    print(str(i) + ';' + result[:-1])
+    fileOutput.write(result)
 fileInput.close()
 fileOutput.close()
